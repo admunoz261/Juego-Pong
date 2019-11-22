f10affc194d72f7de4f39cf953176987608a2af2
Karen Quiceno <ksquiceno3@misena.edu.co>
2019-11-22 18:32:40 -0500

    Realice raquetas y volvi a cambiar el color de la ventana

--- pong.py
+++ pong.py
@@ -1,4 +1,5 @@
 import os, sys, pygame
+from pygame.locals import *
 
 def main():
     pygame.init()
@@ -8,13 +9,22 @@ def main():
     pygame.display.set_caption('Pong Pygame')
 
     purple = (192,122,209)
+    white = (0,0,0)
+
+    velocidad = 2
 
     while 1:
+
+        screen.fill(white)
+        posx = 30
+        posy = 180
+        pygame.draw.rect(screen, purple , (posx, posy, 15, 100))
+        pygame.draw.rect(screen, purple, (755, 100, 15, 100))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                
-        screen.fill(purple)
+                sys(exit)
 
         pygame.display.update()
 
