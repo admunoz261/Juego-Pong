ca8cd3b9efe9f6127fad69edbd8f994c51189d28
Valentina <valen.caldera17@gmail.com>
2019-11-22 17:24:42 -0500

    Creando rama para codigo pelota

--- pong.py
+++ pong.py
@@ -7,11 +7,24 @@ def main():
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Pong Pygame')
 
+    #----------Pelota----------
+    Código de la pelota
+    #--------------------------
+
+
+    pelota = (255, 255, 255)
+    fondo = (128, 128, 128)
+
+
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                return
+                
+        screen.fill(fondo)
+        pygame.draw.circle(screen, pelota, (400, 200), 15)
+        pygame.display.update()
+                
 
 if __name__ == '__main__':
     main()
