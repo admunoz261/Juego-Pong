8a6838a7f3cc1d4ed8bd8af58e866a04e875cc58
Valentina <valen.caldera17@gmail.com>
2019-11-22 15:08:17 -0500

    Añadiendo el main

--- pong.py
+++ pong.py
@@ -1,1 +1,17 @@
-print ("Hola mundo")
+import os, sys, pygame
+
+def main():
+    pygame.init()
+
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Pong Pygame')
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+if __name__ == '__main__':
+    main()
