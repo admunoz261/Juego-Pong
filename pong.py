4be2006ee9e23ffeee4cf6e4df64a8b8643dbfad
Valentina <valen.caldera17@gmail.com>
2019-11-24 19:57:59 -0500

    Añadiendo el movimiento a la pelota y agregando nueva imagen pelota

--- pong.py
+++ pong.py
@@ -11,19 +11,33 @@ def main():
     #Código de la pelota
     #--------------------------
 
-    pelota = (255, 255, 255)
+    #pelota = (255, 255, 255)
+    pelota = pygame.image.load("icon.png")
+    bola = pelota.get_rect()
     fondo = (128, 128, 128)
+    speed = [1, 1]
 
-
+    
     while 1:
+        pos = (400, 200)
+        pygame.time.delay(2)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                
+                pygame.quit()  
+                   
+        bola = bola.move(speed)
+
+        if (bola.left < 0 or bola.right > width):
+            bola = pelota.get_rect(center = pos)
+            
+
+        if (bola.top < 0 or bola.bottom > height):
+            speed[1] = -speed[1]
+
         screen.fill(fondo)
-        pygame.draw.circle(screen, pelota, (400, 200), 15)
-        pygame.display.update()
-                
+        screen.blit(pelota, bola)
+        #pygame.draw.circle(screen, pelota, (400, 200), 15)
+        pygame.display.flip()                
 
 if __name__ == '__main__':
     main()
