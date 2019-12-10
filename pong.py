fd6c332b201a8874e84bee045a717b2f7410a63c
Alejandro Muñoz <admunoz261@misena.edu.co>
2019-12-10 13:58:25 -0500

    añadi el fondo

--- pong.py
+++ pong.py
@@ -7,11 +7,22 @@ def main():
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Pong Pygame')
 
+    blanco = (120,216,235)
+
+
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+        
+        pygame.draw.rect(screen, blanco, (0, 0,15,100))
+        pygame.draw.rect(screen, blanco, (0, 300,15,100))
+        pygame.draw.rect(screen, blanco, (0,385,800,15))
+        pygame.draw.rect(screen, blanco, (0,0,800,15))
+        pygame.draw.rect(screen, blanco, (785, 0,15,100))
+        pygame.draw.rect(screen, blanco, (785, 300,15,100))
+        pygame.display.update()
 
 if __name__ == '__main__':
     main()
